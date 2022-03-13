4f8062d1811b043c5a2e26abfdee98317f4ec496
Mario <mariofdev@gmail.com>
2022-03-13 14:52:56 +0100

    Create users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Res, HttpStatus, Body } from '@nestjs/common';
+import { CreateUserDTO } from './dto/user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post('/create')
+  async createPost(@Res() res, @Body() createUserDTO: CreateUserDTO){
+    try {
+      const user = await this.userService.createUser(createUserDTO);
+
+      return res.status(HttpStatus.OK).json({
+        user
+      });
+      
+    } catch (error) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        error: 'Duplicated username'
+      });
+    }
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { User } from './interfaces/user.interface';
+import { CreateUserDTO } from './dto/user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel('User') private readonly userModel: Model<User>){}
+
+  async createUser(createUserDTO: CreateUserDTO): Promise<User>{
+    const newUser = await new this.userModel(createUserDTO);
+    return newUser.save();
+  }
+}
